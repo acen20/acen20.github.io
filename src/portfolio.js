9b0c06fc3e10d25f0e15e948001a0b1f321eb793
Ahsen Nazir <62377713+acen20@users.noreply.github.com>
2024-09-24 09:17:25 +0500

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "Passionate AI developer 🚀 having experience of developing machine learning and deep learning solutions with Python / PyTorch and many other cool libraries and frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1CbWDCZGl-2gBZR1o4GrWbYIosvNF_kXT/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1SADVFTBi_3X4p7oyjr4DdeTTO1wXM_qh/view?usp=drive_link", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 

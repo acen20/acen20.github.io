3d200f7fe0ffa293ca94e7baa0ae91dfc53c09fd
acen20 <62377713+acen20@users.noreply.github.com>
2026-01-20 10:26:05 +0000

    Deploying to gh-pages from  @ fa4446ff992e4fdd9a4fbfd1185b11ddcff98ef3 🚀

---
+++ precache-manifest.4feec5bd2c4c5564b75575e4a99a200f.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a912d6ec7680d32a1447e5ff22cbf12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67475aa44c4a50e17d95",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "dd5318b6ab59ac70ba5e",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "de4bcca93a2da626a72d",
+    "url": "/static/css/main.5f6dac1c.chunk.css"
+  },
+  {
+    "revision": "54c17137eef902e8e0f2",
+    "url": "/static/js/2.15eb887e.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/static/js/2.15eb887e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67475aa44c4a50e17d95",
+    "url": "/static/js/3.bfcd4787.chunk.js"
+  },
+  {
+    "revision": "dd5318b6ab59ac70ba5e",
+    "url": "/static/js/4.51f7220e.chunk.js"
+  },
+  {
+    "revision": "de4bcca93a2da626a72d",
+    "url": "/static/js/main.fd0f5eaa.chunk.js"
+  },
+  {
+    "revision": "0f2c527a47cb50885fe8",
+    "url": "/static/js/runtime-main.4a6fd3ff.js"
+  },
+  {
+    "revision": "9bdfcb29cf7254a0c71c6c54ff695b76",
+    "url": "/static/media/1medal.9bdfcb29.png"
+  },
+  {
+    "revision": "db29819d5586d0ece122219561055efd",
+    "url": "/static/media/2medal.db29819d.webp"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "f583006cbdc1e73b6802bdcb5d13a834",
+    "url": "/static/media/aitec.f583006c.png"
+  },
+  {
+    "revision": "300b2679112985f79fb70cc9bbc6f073",
+    "url": "/static/media/comfy.300b2679.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8398be8296ef9ce06523d4385b4e46c5",
+    "url": "/static/media/fiverrLogo.8398be82.png"
+  },
+  {
+    "revision": "00c6586d5630ca0f1c325413137d5d1b",
+    "url": "/static/media/ga.00c6586d.png"
+  },
+  {
+    "revision": "c7ac0fff6e7812ccfce60287cbd66c7e",
+    "url": "/static/media/ibm_practitioner.c7ac0fff.png"
+  },
+  {
+    "revision": "b4a5973c1943d17fd72c3dac3c8c0a65",
+    "url": "/static/media/invoice.b4a5973c.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ed5a7c92a7110c1304fc9c74de7b4467",
+    "url": "/static/media/n8n-invoice.ed5a7c92.png"
+  },
+  {
+    "revision": "3783fee3e8a76d9e46a33b5676f5de93",
+    "url": "/static/media/n8n-social.3783fee3.png"
+  },
+  {
+    "revision": "61bf0c397184302ad2cfc1fdfc161113",
+    "url": "/static/media/ncp.61bf0c39.jpg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "f3baf0843f3c4476831f986ccb7963ed",
+    "url": "/static/media/od.f3baf084.png"
+  },
+  {
+    "revision": "02e76c40aec67619a4c770eed8f667aa",
+    "url": "/static/media/pieas.02e76c40.jpg"
+  },
+  {
+    "revision": "75054bf1c5220d43a52fffb35c970659",
+    "url": "/static/media/poster.75054bf1.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "6a549e4b10b38dfa844c040c058e31ee",
+    "url": "/static/media/smartSolLogo.6a549e4b.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);

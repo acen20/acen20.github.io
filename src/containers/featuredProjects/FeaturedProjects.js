cb74a1c27c15093f3bacf21c42618f7b2be0cc3e
acen20 <ahsenmailbox@gmail.com>
2026-02-15 11:42:44 +0500

    updated featured projects

--- src/containers/featuredProjects/FeaturedProjects.js
+++ src/containers/featuredProjects/FeaturedProjects.js
@@ -1,45 +1,28 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import "./FeaturedProjects.scss";
-import {featuredProjects} from "../../portfolio";
-import {Fade} from "react-reveal";
+import { featuredProjects } from "../../portfolio";
 import StyleContext from "../../contexts/StyleContext";
 import ProjectCard from "../../components/projectCard/ProjectCard";
 
 export default function FeaturedProjects() {
-  const {isDark} = useContext(StyleContext);
-  
-  if (!featuredProjects.display) {
-    return null;
-  }
-  
-  return (
-    <Fade bottom duration={1000} distance="20px">
-      <div className="main" id="featured-projects">
-        <div>
-          <h1 className="skills-heading">{featuredProjects.title}</h1>
-          <p
-            className={
-              isDark
-                ? "dark-mode project-subtitle"
-                : "subTitle project-subtitle"
-            }
-          >
-            {featuredProjects.subtitle}
-          </p>
+  const { isDark } = useContext(StyleContext);
 
-          <div className="featured-projects-container">
-            {featuredProjects.projects.map((project, i) => {
-              return (
-                <ProjectCard
-                  key={i}
-                  project={project}
-                  isDark={isDark}
-                />
-              );
-            })}
-          </div>
+  if (!featuredProjects.display) return null;
+
+  return (
+    <div className="main" id="featured-projects">
+      <div className="container">
+        <h1 className="skills-heading">{featuredProjects.title}</h1>
+        <p className={isDark ? "dark-mode project-subtitle" : "subTitle project-subtitle"}>
+          {featuredProjects.subtitle}
+        </p>
+
+        <div className="featured-projects-container">
+          {featuredProjects.projects.map((project, i) => (
+            <ProjectCard key={i} project={project} isDark={isDark} />
+          ))}
         </div>
       </div>
-    </Fade>
+    </div>
   );
 }
